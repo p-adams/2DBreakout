d4a301bc8abfb6c834261b1b71235445a55dac46
p-adams <ghusiyy@gmail.com>
2020-12-25 18:11:49 -0800

    wrap JSX in RecoilRoot and init styling

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,13 @@
 import { FunctionalComponent, h } from "preact";
-
+import { RecoilRoot } from "recoil";
 const App: FunctionalComponent = () => {
-    return <div id="app">2D Breakout</div>;
+    return (
+        <RecoilRoot>
+            <div id="app">
+                <h1>2D Breakout</h1>
+            </div>
+        </RecoilRoot>
+    );
 };
 
 export default App;
