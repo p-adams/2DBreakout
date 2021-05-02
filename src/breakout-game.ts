8d2769570dae4be35b7e9b82e59228113755f41d
p-adams <ghusiyy@gmail.com>
2021-05-02 16:38:37 -0700

    remove boiler plate main file and update breakout component to render simple canvas

--- src/breakout-game.ts
+++ src/breakout-game.ts
@@ -1,9 +1,23 @@
-import { html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { css, html, LitElement } from "lit";
+import { customElement, query } from "lit/decorators.js";
 
 @customElement("breakout-game")
 export class BreakoutGame extends LitElement {
+  static styles = css`
+    canvas {
+      background: #eee;
+    }
+  `;
+
+  @query("canvas")
+  _canvas!: HTMLCanvasElement;
+  firstUpdated() {
+    const ctx = this._canvas.getContext("2d");
+    ctx!.fillStyle = "green";
+    ctx!.fillRect(10, 10, 150, 100);
+  }
+
   render() {
-    return html`<canvas>hello</canvas>`;
+    return html`<canvas width="480" height="320"></canvas>`;
   }
 }
