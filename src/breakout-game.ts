e355512f005f1fb8e5781d19008cab5c4c1bb24c
p-adams <ghusiyy@gmail.com>
2021-05-02 16:08:21 -0700

    init

---
+++ src/breakout-game.ts
@@ -0,0 +1,9 @@
+import { html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("breakout-game")
+export class BreakoutGame extends LitElement {
+  render() {
+    return html`<canvas>hello</canvas>`;
+  }
+}
